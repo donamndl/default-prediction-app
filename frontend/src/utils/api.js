ce70504443d295c0a979b44006c76f979e3bdea8
dona125 <donamndl125@gmail.com>
2026-05-15 11:54:15 +0530

    Connected frontend to backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://default-prediction-application.vercel.app/api',
   headers: { 'Content-Type': 'application/json' }
 })
 
