22d0b87dd9a9c3361c7942078d6be891eff84180
dona125 <donamndl125@gmail.com>
2026-05-15 12:11:00 +0530

    Fixed the backend url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://default-prediction-application.vercel.app/api',
+  baseURL: 'https://default-prediction-app-zsh3.vercel.app/api',
   headers: { 'Content-Type': 'application/json' }
 })
 
